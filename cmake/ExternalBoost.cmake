ec014db97d7de6f8ad675f90b09d60802edf7cd3
Tom Vercauteren <tomvercaut@gmail.com>
2023-01-21 01:17:24 +0100

    Removed unused dependencies.

--- cmake/ExternalBoost.cmake
+++ cmake/ExternalBoost.cmake
@@ -18,17 +18,35 @@ else()
     unset(boost_toolset)
 endif()
 
-ExternalProject_Add(
-        ext_boost
-        GIT_REPOSITORY https://github.com/boostorg/boost.git
-        GIT_TAG boost-1.81.0
-        GIT_SHALLOW ON
-        BUILD_IN_SOURCE 1
-        CONFIGURE_COMMAND ./${boost_bootstrap} --prefix=${CMAKE_INSTALL_PREFIX} --without-python
-        BUILD_COMMAND ./${boost_b2}
+option(use_boost_header_only "Only install the headers of Boost" OFF)
+
+if (${use_boost_header_only})
+    ExternalProject_Add(
+            ext_boost
+            GIT_REPOSITORY https://github.com/boostorg/boost.git
+            GIT_TAG boost-1.81.0
+            GIT_SHALLOW ON
+#            BUILD_IN_SOURCE 1
+            GIT_SUBMODULES_RECURSE ON
+            #            CONFIGURE_COMMAND ./${boost_bootstrap} --prefix=${CMAKE_INSTALL_PREFIX}
+            BUILD_COMMAND ./${boost_b2}
+            --prefix=${CMAKE_INSTALL_PREFIX}
+            headers
+            INSTALL_COMMAND ""
+    )
+else ()
+    ExternalProject_Add(
+            ext_boost
+            GIT_REPOSITORY https://github.com/boostorg/boost.git
+            GIT_TAG boost-1.81.0
+            GIT_SHALLOW ON
+            BUILD_IN_SOURCE 1
+            CONFIGURE_COMMAND ./${boost_bootstrap} --prefix=${CMAKE_INSTALL_PREFIX} --without-python
+            BUILD_COMMAND ./${boost_b2}
             --build-type=complete
             --prefix=${CMAKE_INSTALL_PREFIX}
             toolset=${boost_toolset}
             install
-        INSTALL_COMMAND ""
-)
+            INSTALL_COMMAND ""
+    )
+endif ()
--- cmake/ExternalRtpConnectTools.cmake
+++ cmake/ExternalRtpConnectTools.cmake
@@ -4,14 +4,16 @@ ExternalProject_Add(
     ext_rtp_connect_tools
     GIT_REPOSITORY https://github.com/tomvercaut/rtp_connect_tools.git
     GIT_TAG main
-    DEPENDS ext_catch2 ext_boost ext_spdlog ext_fmt ext_tomlplusplus ext_wxwidgets
+    DEPENDS ext_catch2 ext_cli11 ext_spdlog ext_fmt ext_tomlplusplus
     CMAKE_ARGS
-            -DBoost_DIR:PATH=${CMAKE_INSTALL_PREFIX}/lib/cmake/Boost-1.81.0
             -Dspdlog_DIR:PATH=${CMAKE_INSTALL_PREFIX}/lib/cmake/spdlog
             -Dfmt_DIR:PATH=${CMAKE_INSTALL_PREFIX}/lib/cmake/fmt
             -Dtomlplusplus_DIR:PATH=${CMAKE_INSTALL_PREFIX}/lib/cmake/tomlplusplus
             -DCatch2_DIR:PATH=${CMAKE_INSTALL_PREFIX}/lib/cmake/Catch2
-            -DwxWidgets_ROOT_DIR:PATH=${CMAKE_INSTALL_PREFIX}/lib/cmake/wxWidgets
+            -DCLI11_DIR:PATH=${CMAKE_INSTALL_PREFIX}/share/cmake/CLI11
             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
             -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
 )
+#   DEPENDS ext_boost ext_wxwidgets
+#            -DBoost_DIR:PATH=${CMAKE_INSTALL_PREFIX}/lib/cmake/Boost-1.81.0
+#            -DwxWidgets_ROOT_DIR:PATH=${CMAKE_INSTALL_PREFIX}/lib/cmake/wxWidgets
